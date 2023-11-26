2ab1d3715ed9f75c180c9d3ea9918a8e8cdbc092
dpoxodp <bhasseldal@gmail.com>
2023-11-26 16:08:21 +0000

    created provider for terraform

---
+++ infra/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.26.0"
+        }
+    }
+    backend "s3" {
+        bucket = "pgr301-2021-terraform-state"
+        key = "2009-jams/apprunner-a-new-state.state"
+        region = "eu-north-1"
+    }
+}
